c2525b90f4b25c229ac08be3e1154232e9234d6a
Baranov KA <baranovka@quarta.su>
2018-01-31 11:37:39 +0300

    Главная и контакты

---
+++ sv-dom/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="sv_dom.MvcApplication" Language="C#" %>
---
+++ sv-dom/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(sv_dom.Startup))]
+namespace sv_dom
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
