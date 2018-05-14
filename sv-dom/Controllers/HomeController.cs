3a841d861b0a0fac852c77fa7952a8790c306f9d
Kirill Baranov <kirill117@yandex.ru>
2018-05-14 23:08:05 +0300

    Системный комментарий в форме обратной связи

--- sv-dom/Controllers/HomeController.cs
+++ sv-dom/Controllers/HomeController.cs
@@ -151,7 +151,7 @@ namespace sv_dom.Controllers
             return Json(new { cost = result, cost1 = result1, cost2 = result2, cost3 = result3  });
         }
 
-        public JsonResult GetRecall(string name, string phone, string email, string comment)
+        public JsonResult GetRecall(string name, string phone, string email, string comment, string subcomment)
         {
             var result = false;
             var user = ConfigurationManager.AppSettings["MailUserTarget"];
@@ -165,6 +165,11 @@ namespace sv_dom.Controllers
                 if (!string.IsNullOrEmpty(email))
                 {
                     body.AppendLine("Email: " + email.Trim());
+                }
+                if (!string.IsNullOrEmpty(subcomment))
+                {
+                    body.AppendLine("");
+                    body.AppendLine(subcomment.Trim());
                 }
                 if (!string.IsNullOrEmpty(comment))
                 {
