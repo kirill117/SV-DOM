e831398e40c96b4baa748dc2b76442c8d86acb6f
Baranov KA <baranovka@quarta.su>
2018-06-19 13:59:41 +0300

    Инженерия

--- sv-dom/Controllers/HomeController.cs
+++ sv-dom/Controllers/HomeController.cs
@@ -121,6 +121,9 @@ namespace sv_dom.Controllers
                 case 6:
                     name += "\\Roof";
                     break;
+                case 7:
+                    name += "\\Engineering";
+                    break;
             }
 
             return View(name);
